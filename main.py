f3c5d645555ec20260b535b203c3633f64761a98
Sai Venkata Siddardha Kasam <157007052+siddu28@users.noreply.github.com>
2025-11-27 19:21:57 +0530

    data ingestion pipeline completed

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f"stage {STAGE_NAME} started") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f"stage {STAGE_NAME} completed!")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
--- src/datascience/utils/common.py
+++ src/datascience/utils/common.py
@@ -12,18 +12,6 @@ from box.exceptions import BoxValueError
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns
-
-    Args:
-        path_to_yaml (str): path like input
-
-    Raises:
-        ValueError: if yaml file is empty
-        e: empty file
-
-    Returns:
-        ConfigBox: ConfigBox type
-    """
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
@@ -38,12 +26,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories
 
-    Args:
-        path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
-    """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
@@ -51,12 +34,7 @@ def create_directories(path_to_directories: list, verbose=True):
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
-    """save json data
 
-    Args:
-        path (Path): path to json file
-        data (dict): data to be saved in json file
-    """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
@@ -64,14 +42,7 @@ def save_json(path: Path, data: dict):
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
-    """load json files data
 
-    Args:
-        path (Path): path to json file
-
-    Returns:
-        ConfigBox: data as class attributes instead of dict
-    """
     with open(path) as f:
         content = json.load(f)
 
@@ -80,25 +51,13 @@ def load_json(path: Path) -> ConfigBox:
 
 @ensure_annotations
 def save_bin(data: Any, path: Path):
-    """save binary file
 
-    Args:
-        data (Any): data to be saved as binary
-        path (Path): path to binary file
-    """
     joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at: {path}")
 
 @ensure_annotations
 def load_bin(path: Path) -> Any:
-    """load binary data
 
-    Args:
-        path (Path): path to binary file
-
-    Returns:
-        Any: object stored in the file
-    """
     data = joblib.load(path)
     logger.info(f"binary file loaded from: {path}")
     return data
