9196be1f969b3fe270f9689e136f55efe6ab4818
Sai Venkata Siddardha Kasam <157007052+siddu28@users.noreply.github.com>
2025-11-28 19:37:04 +0530

    Data validation completed

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,7 +1,10 @@
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 
-from src.datascience.entity.config_entity import DataIngestionConfig
+from src.datascience.entity.config_entity import DataIngestionConfig,DataValidationConfig
+
+
+
 class ConfigurationManager:
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
@@ -26,4 +29,20 @@ class ConfigurationManager:
 
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self)->DataValidationConfig:
+
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema
+        )
+
+        return data_validation_config
     
