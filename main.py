ede1be821dd15b42e833254e6044147c87412374
Sai Venkata Siddardha Kasam <157007052+siddu28@users.noreply.github.com>
2025-11-29 16:33:09 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTraini
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainingPipeline
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -43,4 +44,17 @@ try:
    obj.initiate_model_training()
    logger.info(f"stage {STAGE_NAME} completed!")
 except Exception as e:
+   raise e
+
+
+
+STAGE_NAME="Model Evaluation Stage"
+
+try:
+   logger.info(f"stage {STAGE_NAME} started.....")
+   obj = ModelEvaluationPipeline()
+   obj.model_evaluation()
+   logger.info(f"stage {STAGE_NAME} completed!")
+except Exception as e:
+   logger.exception(e)
    raise e
---
+++ src/datascience/components/model_evaluation.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+from sklearn.metrics import mean_absolute_error,mean_squared_error,mean_absolute_error,r2_score
+import mlflow
+import numpy as np
+import joblib
+from pathlib import Path
+from src.datascience.config.configuration import ModelEvaluationConfig
+from src.datascience.utils.common import save_json
+
+import os
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/siddukasam28/PracticeDataScienceProject.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "siddukasam28"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "63c4cbbeb538d23123b3424e131fc50146474157"
+
+class ModelEvaluation:
+    def __init__(self,config=ModelEvaluationConfig):
+        self.config = config
+
+    def eval_metrics(self,actual,pred):
+        rmse = np.sqrt(mean_squared_error(actual,pred))
+        mae = mean_absolute_error(actual,pred)
+        r2 = r2_score(actual,pred)
+        return rmse,mae,r2
+    
+    def log_into_mlflow(self):
+        test_data = pd.read_csv(self.config.test_data_path)
+        model = joblib.load(self.config.model_path)
+
+        test_x = test_data.drop([self.config.target_column],axis=1)
+        test_y = test_data[[self.config.target_column]]
+
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+
+        with mlflow.start_run():
+
+            predicted_qualities = model.predict(test_x)
+            (rmse,mae,r2) = self.eval_metrics(test_y,predicted_qualities)
+
+            scores = {"rmse":rmse,"mae":mae,"r2":r2}
+            save_json(path = Path(self.config.metric_file_name),data=scores)
+
+            mlflow.log_params(self.config.all_params)
+            mlflow.log_metric("rmse",rmse)
+            mlflow.log_metric("r2",r2)
+
+            os.makedirs("model",exist_ok=True)
+            joblib.dump(model,"model/model.pkl")
+
+            mlflow.log_artifacts("model",artifact_path="model")
